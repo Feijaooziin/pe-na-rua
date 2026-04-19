ec7c1b364d98eac7655371273be5482d24cc7c3e
feijaoozin <gonardo1234@gmail.com>
2026-04-19 06:27:03 -0300

    fix: arrumando bugs de rotas

--- app/(drawer)/settings.tsx
+++ app/(drawer)/settings.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Map() {
+export default function Settings() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>WORK IN PROGRESS 🗺️</Text>
