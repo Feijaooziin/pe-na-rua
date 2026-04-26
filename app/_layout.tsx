8cec0b6339b9955388d461902aa03e8515e6162d
feijaoozin <gonardo1234@gmail.com>
2026-04-25 23:18:00 -0300

    fix: arrumando layout pra criar a db

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
+import { initDB } from "@/src/database/db";
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Layout() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    initDB();
+    setReady(true);
+  }, []);
+
+  if (!ready) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
