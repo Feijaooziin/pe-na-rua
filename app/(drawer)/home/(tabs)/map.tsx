4aba9d18aa85a48d5253c4d98fdc49110875f953
feijaoozin <gonardo1234@gmail.com>
2026-04-26 14:43:37 -0300

    feat: iniciando mapa

--- app/(drawer)/home/(tabs)/map.tsx
+++ app/(drawer)/home/(tabs)/map.tsx
@@ -1,20 +1,85 @@
-import Header from "@/src/components/Header";
-import { colors } from "@/src/theme/colors";
+import { useFocusEffect } from "@react-navigation/native";
+import * as Location from "expo-location";
+import { useCallback, useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+
+import Header from "@/src/components/Header";
+import { getTrees } from "@/src/database/trees";
+import { Tree } from "@/src/types/tree";
 
 export default function Map() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null,
+  );
+  const [trees, setTrees] = useState<Tree[]>([]);
+
+  // 📍 pegar localização
+  useEffect(() => {
+    (async () => {
+      const { granted } = await Location.requestForegroundPermissionsAsync();
+
+      if (!granted) {
+        alert("Permissão de localização negada");
+        return;
+      }
+
+      const currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+    })();
+  }, []);
+
+  // 🌳 pegar árvores do banco
+  useFocusEffect(
+    useCallback(() => {
+      const data = getTrees();
+      setTrees(data);
+    }, []),
+  );
+
+  if (!location) {
+    return <Text>Carregando mapa...</Text>;
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <Header />
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          backgroundColor: colors.background,
+
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
         }}
       >
-        <Text>WORK IN PROGRESS 🗺️</Text>
-      </View>
+        {/* 📍 Local atual */}
+        <Marker
+          coordinate={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+          }}
+          title="Você está aqui"
+        />
+
+        {/* 🌳 Árvores */}
+        {trees.map((tree) => {
+          if (!tree.latitude || !tree.longitude) return null;
+
+          return (
+            <Marker
+              key={tree.id}
+              coordinate={{
+                latitude: tree.latitude,
+                longitude: tree.longitude,
+              }}
+              title={tree.name}
+              description={tree.description}
+            />
+          );
+        })}
+      </MapView>
     </View>
   );
 }
