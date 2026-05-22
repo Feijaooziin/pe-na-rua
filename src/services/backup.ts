12a789ce1eabf07c4fb5f85a75082a069ddac7fb
feijaoozin <gonardo1234@gmail.com>
2026-05-21 23:24:55 -0300

    fix: apagando lixo

--- src/services/backup.ts
+++ src/services/backup.ts
@@ -1,36 +1,3 @@
-// import * as FileSystem from "expo-file-system/legacy";
-// import * as Sharing from "expo-sharing";
-
-// import { getTrees } from "@/src/database/trees";
-
-// export async function exportTrees() {
-//   try {
-//     // 🌳 pegar árvores
-//     const trees = getTrees();
-
-//     // 🧹 remover IDs
-//     const cleanTrees = trees.map(({ id, ...rest }) => rest);
-
-//     // 📄 converter para JSON
-//     const json = JSON.stringify(cleanTrees, null, 2);
-
-//     // 📁 caminho do arquivo
-//     const fileUri = FileSystem.documentDirectory + "pe-na-rua-backup.json";
-
-//     // 💾 salvar arquivo
-//     await FileSystem.writeAsStringAsync(fileUri, json, {
-//       encoding: FileSystem.EncodingType.UTF8,
-//     });
-
-//     // 📤 compartilhar arquivo
-//     await Sharing.shareAsync(fileUri);
-
-//     console.log("Backup exportado:", fileUri);
-//   } catch (error) {
-//     console.log("Erro ao exportar backup", error);
-//   }
-// }
-
 import * as DocumentPicker from "expo-document-picker";
 import * as FileSystem from "expo-file-system/legacy";
 import * as Sharing from "expo-sharing";
